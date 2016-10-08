ad40bfa1c3ef6cec0672675588a153bb54a83412
estebanpw <estebanpw@uma.es>
2016-10-08 15:18:32 +0200

    improved formatting

--- basic_starter.c
+++ basic_starter.c
@@ -25,32 +25,32 @@ void terror(const char * msg){
 
 int main(int argc, char ** av){
     
-    // Configuration variables
-    
-    FILE * query, * db, * out;
-    int ksize = -1;
-    int verbose = 0;
-    
-    int pNum = 0;
-    while(pNum < argc){
-    	if(strcmp(av[pNum], "-verbose") == 0) verbose = 1;
-    	if(strcmp(av[pNum], "-query") == 0){
-    		query = fopen64(av[pNum+1], "rt");
-    		if(query==NULL) terror("Could not open query file");
+	// Configuration variables
+	
+	FILE * query, * db, * out;
+	int ksize = -1;
+	int verbose = 0;
+	
+	int pNum = 0;
+	while(pNum < argc){
+		if(strcmp(av[pNum], "-verbose") == 0) verbose = 1;
+		if(strcmp(av[pNum], "-query") == 0){
+			query = fopen64(av[pNum+1], "rt");
+			if(query==NULL) terror("Could not open query file");
 		}
 		if(strcmp(av[pNum], "-db") == 0){
-    		db = fopen64(av[pNum+1], "rt");
-    		if(db==NULL) terror("Could not open database file");
+			db = fopen64(av[pNum+1], "rt");
+			if(db==NULL) terror("Could not open database file");
 		}
 		if(strcmp(av[pNum], "-out") == 0){
-    		out = fopen64(av[pNum+1], "wt");
-    		if(out==NULL) terror("Could not open output file");
+			out = fopen64(av[pNum+1], "wt");
+			if(out==NULL) terror("Could not open output file");
 		}
-    	if(strcmp(av[pNum], "-ksize") == 0){
-    		ksize = atoi(av[pNum+1]);
-    		if(ksize < 1) terror("Ksize must be positive");
+		if(strcmp(av[pNum], "-ksize") == 0){
+			ksize = atoi(av[pNum+1]);
+			if(ksize < 1) terror("Ksize must be positive");
 		}
-    	pNum++;
+		pNum++;
 	}
 	
 	// Exit if not all needed parameters were included
@@ -58,14 +58,14 @@ int main(int argc, char ** av){
 	if(query==NULL || db==NULL || out==NULL || ksize==-1) terror("A query, database, outputfile and ksize value is required");
 	
 	// Finished configuration variables step
-    
-    // Close files, free memory
-    
-    fclose(query);
-    fclose(db);
-    fclose(out);
-    
- 	return 0;   
+	
+	// Close files, free memory
+	
+	fclose(query);
+	fclose(db);
+	fclose(out);
+	
+	return 0;
 }
 
 
